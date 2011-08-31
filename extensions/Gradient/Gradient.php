3f79b4a9ccc88eefa730848f15d913acff3aceda
pdclark <pdclark@pdclark.com>
2011-08-30 23:04:42 -0500

    Gradient preview

--- extensions/Gradient/Gradient.php
+++ extensions/Gradient/Gradient.php
@@ -74,15 +74,23 @@ class PDStyles_Extension_Gradient extends PDStyles_Extension_Observer {
 	
 	function output_inner() {
 		?>
-		<input class="pds_color_input grad_color" type="text" name="<?php echo $this->form_name ?>[from]" id="<?php echo $this->form_id ?>" value="<?php echo $this->value('form', 'from'); ?>" size="8" maxlength="8" />
-		<input class="pds_color_input grad_color" type="text" name="<?php echo $this->form_name ?>[to]" id="<?php echo $this->form_id ?>" value="<?php echo $this->value('form', 'to'); ?>" size="8" maxlength="8" />
+		<div class="colors">
+			<input class="pds_color_input grad_color" type="text" name="<?php echo $this->form_name ?>[from]" id="<?php echo $this->form_id ?>" value="<?php echo $this->value('form', 'from'); ?>" size="8" maxlength="8" />
+			<input class="pds_color_input grad_color" type="text" name="<?php echo $this->form_name ?>[to]" id="<?php echo $this->form_id ?>" value="<?php echo $this->value('form', 'to'); ?>" size="8" maxlength="8" />
+		</div>
 		<div>Stops: <input class="pds_text_input stops" type="text" name="<?php echo $this->form_name ?>[stops]" id="<?php echo $this->form_id ?>" value="<?php echo $this->value('form', 'stops'); ?>" size="32" /></div>
 		
 		<style>
-			.stop-markers { position:relative;width: 500px;margin-top:20px;height: 50px;}
-			.stop-markers .marker {position:absolute;top:0;left:0;width: 500px;height: 0px;border:none;}
+			.stop-markers, .preview {width: 500px;}
+			.stop-markers { position:relative;;margin-top:5px;height: 80px;}
+			.stop-markers .marker {position:absolute;top:50px;left:0;width: 500px;height: 0px;border:none;}
 			.stop-markers .marker a {background-image:url();}
+			.preview {height: 45px;display:block;border: 1px solid black;}
+/*			.pds_gradient .colors {display:none;}*/
 		</style>
+		
+		
+		<div class="preview"></div>
 		<div class="stop-markers">
 			<div class="marker"></div>
 			<div class="marker"></div>
