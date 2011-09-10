a0755fafb3a16c08af5e094245e171ee6f20bbaa
pdclark <pdclark@pdclark.com>
2011-09-10 18:39:15 -0500

    Version bump

--- styles.php
+++ styles.php
@@ -3,7 +3,7 @@
 Plugin Name: Styles
 Plugin URI: http://brainstormmedia.com
 Description: Less code, more style.
-Version: 0.3.0
+Version: 0.3.1
 Author: Brainstorm Media
 Author URI: http://brainstormmedia.com
 
@@ -91,7 +91,7 @@ class StormStyles extends Scaffold_Extension_Observable {
 	 * @since 0.1
 	 * @var int
 	 **/
-	var $version = '0.3.0';
+	var $version = '0.3.1';
 	
 	/**
 	 * Plugin DB version
@@ -101,7 +101,7 @@ class StormStyles extends Scaffold_Extension_Observable {
 	 * 
 	 * @var int
 	 **/
-	var $db_version = '0.3.0';
+	var $db_version = '0.3.1';
 	
 	/**
 	 * Options array containing all options for this plugin
