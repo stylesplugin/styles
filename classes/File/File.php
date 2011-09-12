638c563627fd19ca03d46ffeac97283d242c80a6
pdclark <pdclark@pdclark.com>
2011-09-12 03:47:59 -0500

    Restore old ajax_update method

--- classes/File/File.php
+++ classes/File/File.php
@@ -36,7 +36,6 @@ class StormStyles_Extension_File {
 		$this->active_id = $blog_id;
 
 		$cached_file = str_replace('.scss', '.css', $file);
-		$no_cache = false;
 
 		if ( !file_exists( get_stylesheet_directory().$file ) ) {
 			FB::error( 'File not found: '.get_stylesheet_directory().$file );
@@ -46,27 +45,27 @@ class StormStyles_Extension_File {
 		// Check for cached output
 		if ( !file_exists( get_stylesheet_directory().$cached_file ) ) {
 			FB::error( 'Cached output not found: '.get_stylesheet_directory().$cached_file );
-			$no_cache = true;
+			$have_cache = false;
+		}else {
+			$have_cache = true;
 		}
 
 		if ( is_admin() || isset( $_GET['scaffold'] ) || 'admin-ajax.php' == basename($_SERVER['PHP_SELF']) ) {
 			// Load up variables from SCSS if we're in Admin or running a cache save via AJAX
 			$this->active_file = new StormStyles_Extension_Variable( array(
 	 			'file' => get_stylesheet_directory().$file, // Absolute path
-				'cache_file' => get_stylesheet_directory().$cached_file,
+				'CACHE_FILE' => get_stylesheet_directory().$cached_file,
 				'permalink' => $this->active_id,
 			) );
 			
 		}else {
-			
-			if ( $no_cache || ( defined('BSM_DEVELOPMENT') && true === BSM_DEVELOPMENT ) ) {
+			if ( $have_cache && BSM_DEVELOPMENT !== true) {
+				// Enqueue cached output
+				wp_enqueue_style('bsm-scaffold', get_stylesheet_directory_uri().$cached_file );
+			}else {
 				// If we're developing, force re-render on every CSS load
 				// Pairs well with: if ( '127.0.0.1' == $_SERVER['SERVER_ADDR'] ) {define('BSM_DEVELOPMENT', true);}
 				wp_enqueue_style('bsm-scaffold', '/?scaffold', array(), time() );
-				
-			}else {
-				// Enqueue cached output if we're in frontend
-				wp_enqueue_style('bsm-scaffold', get_stylesheet_directory_uri().$cached_file );
 			}
 		}
 		
