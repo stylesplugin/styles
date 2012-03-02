97ca27218d58f11bb6721d186f18c77fd95f81a9
pdclark <pdclark@pdclark.com>
2012-03-02 03:36:24 -0600

    Change 'Insert into Post' to 'Select Image'

--- js/admin-media-upload.js
+++ js/admin-media-upload.js
@@ -1,6 +1,8 @@
 jQuery(function($) {
 	$('#sidemenu a').each(function(){
-		var href = $(this).attr('href')+'&StormStyles=1';
+		var href = $(this).attr('href')+'&styles=1';
 		$(this).attr('href', href );
 	});
+
+	$('td.savesend input[value="Insert into Post"]').val('Select Image').addClass('button-primary');
 });
