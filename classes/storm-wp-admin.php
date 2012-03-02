2dfd9164230e86bde3614b79c0518ad9b1af002d
pdclark <pdclark@pdclark.com>
2012-03-02 00:51:18 -0600

    Recache CSS on theme switch

--- classes/storm-wp-admin.php
+++ classes/storm-wp-admin.php
@@ -51,13 +51,16 @@ class Storm_WP_Admin extends Storm_WP_Frontend {
 		// load_plugin_textdomain ( 'styles', false, 'styles/localization' );
         
 		// Activation hook
-		register_activation_hook ( $this->plugin_file, array( $this, 'init' ) );
+		register_activation_hook( $this->plugin_file, array( $this, 'init' ) );
         
 		// Whitelist options
-		add_action ( 'admin_init', array( $this, 'register_settings' ) );
+		add_action( 'admin_init', array( $this, 'register_settings' ) );
         
 		// Activate the options page
-		add_action ( 'admin_menu', array( $this, 'add_page' ) ) ;
+		add_action( 'admin_menu', array( $this, 'add_page' ) );
+		
+		// Rebuild CSS on theme switch
+		add_action( 'switch_theme', array($this, 'force_recache') );
         
 		// AJAX
 		add_action('wp_ajax_styles-update-options', array( $this, 'update_ajax') );
@@ -148,161 +151,6 @@ class Storm_WP_Admin extends Storm_WP_Frontend {
 		wp_enqueue_script ( 'styles-media-upload', $this->plugin_url().'/js/admin-media-upload.js', array('jquery'), $this->version );
 	}
 	
-	/**
-	 * Return a list of languages available
-	 *
-	 * @author Matt Martz <matt@sivel.net>
-	 * @return array
-	 */
-	function languages() {
-		$languages = array( 
-			'ar',    // Arabic
-			'ca',    // Catalan
-			'cs',    // Czech
-			'da',    // Danish
-			'de-CH', // Swiss German
-			'de-DE', // German
-			'en',    // English
-			'es',    // Spanish
-			'et',    // Estonian
-			'fi',    // Finnish
-			'fr',    // French
-			'gl',    // Galician 
-			'he',    // Hebrew
-			'hu',    // Hungarian
-			'id',    // Indonesian
-			'is',    // Icelandic
-			'it',    // Italian
-			'ja',    // Japanese
-			'ko',    // Korean
-			'my',    // Burmese 
-			'nl',    // Dutch
-			'no',    // Norwegian
-			'pl',    // Polish
-			'pt-BR', // Brazilian Portuguese
-			'pt-PT', // Portuguese
-			'ro',    // Romanian
-			'ru',    // Rusian
-			'sk',    // Slovak
-			'sv',    // Swedish
-			'tr',    // Turkish
-			'zh-CN', // Chinese (Simplified)
-			'zh-TW'   // Chinese (Traditional)
-		);
-		return $languages;
-	}
-	
-	/**
-	 * Try to set pdstyles language based on defined language for WordPress
-	 *
-	 * @author Matt Martz <matt@sivel.net>
-	 * @return string
-	 */
-	function set_lang() {
-		if ( defined ( 'WPLANG' ) )
-			$wp_lang = WPLANG;
-		else
-			$wp_lang = 'en';
-	
-		switch ( $wp_lang ) {
-			case 'ar' :
-				$lang = 'ar';    // Arabic
-				break;
-			case 'ca' :
-				$lang = 'ca';    // Catalan
-				break;
-			case 'cs_CZ' :
-				$lang = 'cs';    // Czech
-				break;
-			case 'da_DK' :
-				$lang = 'da';    // Danish
-				break;
-			case 'de_DE' :
-				$lang = 'de-DE'; // German
-				break;
-			case 'es_ES' :
-				$lang = 'es';    // Spanish
-				break;
-			case 'et' :
-				$lang = 'et';    // Estonian
-				break;
-			case 'fi' :
-			case 'fi_FI':
-				$lang = 'fi';    // Finnish
-				break;
-			case 'fr_BE' :
-			case 'fr_FR' :
-				$lang = 'fr';    // French
-				break;
-			case 'gl_ES' :
-				$lang = 'gl';    // Galician
-				break;
-			case 'he_IL' :
-				$lang = 'he';    // Hebrew
-				break;
-			case 'hu_HU' :
-				$lang = 'hu';    // Hungarian
-				break;
-			case 'id_ID' :
-				$lang = 'id';    // Indonesian
-				break;
-			case 'is_IS' :
-				$lang = 'is';    // Icelandic
-				break;
-			case 'it_IT' :
-				$lang = 'it';    // Italian
-				break;
-			case 'ja' :
-				$lang = 'ja';    // Japanese
-				break;
-			case 'ko_KR' :
-				$lang = 'ko';    // Korean
-				break;
-			case 'my_MM' :
-				$lang = 'my';    // Burmese
-				break;
-			case 'nl' :
-			case 'nl_NL' :
-				$lang = 'nl';    // Dutch
-				break;
-			case 'nn_NO' :
-				$lang = 'no';    // Norwegian
-				break;
-			case 'pl_PL' :
-				$lang = 'pl';    // Polish
-				break;
-			case 'pt_BR' :
-				$lang = 'pt-BR'; // Brazilian Portuguese
-				break;
-			case 'pt_PT' :
-				$lang = 'pt-PT'; // Portuguese
-				break;
-			case 'ro' :
-				$lang = 'ro';    // Romanian
-				break;
-			case 'ru_RU' :
-			case 'ru_UA' :
-				$lang = 'ru';    // Rusian
-				break;
-			case 'sk' :
-				$lang = 'sk';    // Slovak
-				break;
-			case 'sv_SE' :
-				$lang = 'sv';    // Swedish
-				break;
-			case 'tr' :
-				$lang = 'tr';    // Turkish
-				break;
-			case 'zh_CN' :
-				$lang = 'zh-CN'; // Chinese (Simplified)
-				break;
-			default :
-				$lang = 'en';    // English
-				break;
-		}
-		return $lang;
-	}
-
 	/**
 	 * Return the default options
 	 *
@@ -428,6 +276,34 @@ class Storm_WP_Admin extends Storm_WP_Frontend {
 	}
 	
 	/**
+	 * Rebuild and recache CSS. e.g., on theme switch
+	 */
+	function force_recache() {
+		do_action('styles_init', $this->styles);
+		do_action('styles_before_process', $this->styles);
+		do_action('styles_process', $this->styles);
+		do_action('styles_after_process', $this->styles);
+		
+		$this->write_to_cache();
+	}
+	
+	/**
+	 * Write... to the cache.
+	 */
+	function write_to_cache() {
+		$cache_file = $this->styles->file_paths['cache_path'];
+		$cache_nicename = str_replace(ABSPATH, '/', $cache_file);
+
+		if ( $cache_file !== false && @file_put_contents($cache_file, $this->styles->css->contents) ) {
+			// Cache written to file
+			return 'Stylesheet rendered and cached to <a href="'.site_url().$cache_nicename.'">'.$cache_nicename.'</a>.<br/>';
+		}else {
+			update_option( 'styles-cache', '/* Styles outputted inline because cache directory "'.$cache_nicename.'" is not writable */'."\r". Minify_CSS_Compressor::process($this->styles->css->contents) );
+			return '<div>Could not write to  <code>'.$cache_nicename.'</code> directory.<br/> CSS has been cached to the database instead. This can be changed by making the directory writable with <code>chmod 666</code></div>';
+		}
+	}
+	
+	/**
 	 * Handle updating options via AJAX; cache Scaffold output
 	 * 
 	 * @since 0.1
@@ -453,17 +329,8 @@ class Storm_WP_Admin extends Storm_WP_Frontend {
 				$response['message'] .= 'Variables unchanged.<br/>';
 			}
 
-			$cache_file = $this->styles->file_paths['cache_path'];
-			$cache_nicename = str_replace(ABSPATH, '/', $cache_file);
+			$response['message'] .= $this->write_to_cache();
 
-			if ( $cache_file !== false && @file_put_contents($cache_file, $this->styles->css->contents) ) {
-				// Cache written to file
-				$response['message'] .= 'Stylesheet rendered and cached to <a href="'.site_url().$cache_nicename.'">'.$cache_nicename.'</a>.<br/>';
-			}else {
-				$response['message'] = '<div>Could not write to  <code>'.$cache_nicename.'</code> directory.<br/> CSS has been cached to the database instead. This can be changed by making the directory writable with <code>chmod 666</code></div>';
-				update_option( 'styles-cache', '/* Styles outputted inline because cache directory "'.$cache_nicename.'" is not writable */'."\r". Minify_CSS_Compressor::process($this->styles->css->contents) );
-			}
-			
 		}
 		
 		$response['message'] = '<div class="'.$class.' settings-error" id="setting-error-settings_updated"> 
@@ -490,7 +357,6 @@ class Storm_WP_Admin extends Storm_WP_Frontend {
 			add_action ( "admin_print_scripts-{$this->options_page_hookname}", array( $this, 'admin_js' ) );
 			add_action ( "admin_print_styles-{$this->options_page_hookname}", array( $this, 'admin_css' ) );
 			add_filter ( "plugin_action_links_{$this->plugin_basename}", array( $this, 'filter_plugin_actions' ) );
-
 		}
 	}
 	
@@ -527,6 +393,102 @@ class Storm_WP_Admin extends Storm_WP_Frontend {
 		}
 
 		$this->load_view('admin.php');
+	}
+	
+	/**
+	 * Return a list of languages available
+	 *
+	 * @author Matt Martz <matt@sivel.net>
+	 * @return array
+	 */
+	function languages() {
+		$languages = array( 
+			'ar',    // Arabic
+			'ca',    // Catalan
+			'cs',    // Czech
+			'da',    // Danish
+			'de-CH', // Swiss German
+			'de-DE', // German
+			'en',    // English
+			'es',    // Spanish
+			'et',    // Estonian
+			'fi',    // Finnish
+			'fr',    // French
+			'gl',    // Galician 
+			'he',    // Hebrew
+			'hu',    // Hungarian
+			'id',    // Indonesian
+			'is',    // Icelandic
+			'it',    // Italian
+			'ja',    // Japanese
+			'ko',    // Korean
+			'my',    // Burmese 
+			'nl',    // Dutch
+			'no',    // Norwegian
+			'pl',    // Polish
+			'pt-BR', // Brazilian Portuguese
+			'pt-PT', // Portuguese
+			'ro',    // Romanian
+			'ru',    // Rusian
+			'sk',    // Slovak
+			'sv',    // Swedish
+			'tr',    // Turkish
+			'zh-CN', // Chinese (Simplified)
+			'zh-TW'  // Chinese (Traditional)
+		);
+		return $languages;
+	}
+	
+	/**
+	 * Try to set pdstyles language based on defined language for WordPress
+	 *
+	 * @author Matt Martz <matt@sivel.net>
+	 * @return string
+	 */
+	function set_lang() {
+		if ( defined ( 'WPLANG' ) )
+			$wp_lang = WPLANG;
+		else
+			$wp_lang = 'en';
+	
+		switch ( $wp_lang ) {
+			case 'ar':    $lang = 'ar';    break; // Arabic
+			case 'ca':    $lang = 'ca';    break; // Catalan
+			case 'cs_CZ': $lang = 'cs';    break; // Czech
+			case 'da_DK': $lang = 'da';    break; // Danish
+			case 'de_DE': $lang = 'de-DE'; break; // German
+			case 'es_ES': $lang = 'es';    break; // Spanish
+			case 'et':    $lang = 'et';    break; // Estonian
+			case 'fi':
+			case 'fi_FI': $lang = 'fi';    break; // Finnish
+			case 'fr_BE': 
+			case 'fr_FR': $lang = 'fr';    break; // French
+			case 'gl_ES': $lang = 'gl';    break; // Galician
+			case 'he_IL': $lang = 'he';    break; // Hebrew
+			case 'hu_HU': $lang = 'hu';    break; // Hungarian
+			case 'id_ID': $lang = 'id';    break; // Indonesian
+			case 'is_IS': $lang = 'is';    break; // Icelandic
+			case 'it_IT': $lang = 'it';    break; // Italian
+			case 'ja':    $lang = 'ja';    break; // Japanese
+			case 'ko_KR': $lang = 'ko';    break; // Korean
+			case 'my_MM': $lang = 'my';    break; // Burmese
+			case 'nl':
+			case 'nl_NL': $lang = 'nl';    break; // Dutch
+			case 'nn_NO': $lang = 'no';    break; // Norwegian
+			case 'pl_PL': $lang = 'pl';    break; // Polish
+			case 'pt_BR': $lang = 'pt-BR'; break; // Brazilian Portuguese
+			case 'pt_PT': $lang = 'pt-PT'; break; // Portuguese
+			case 'ro':    $lang = 'ro';    break; // Romanian
+			case 'ru_RU':
+			case 'ru_UA': $lang = 'ru';    break; // Rusian
+			case 'sk':    $lang = 'sk';    break; // Slovak
+			case 'sv_SE': $lang = 'sv';    break; // Swedish
+			case 'tr':    $lang = 'tr';    break; // Turkish
+			case 'zh_CN': $lang = 'zh-CN'; break; // Chinese (Simplified)
+			default :     $lang = 'en';    break; // English
+ 
+		}
+		return $lang;
 	}
 
 } // END class StormStylesAdminController extends StormStyles
