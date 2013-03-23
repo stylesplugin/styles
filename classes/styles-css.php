a137b4e41e42956f41bf3578c943323d5bef3112
Paul Clark <pdclark@brainstormmedia.com>
2013-03-23 00:56:27 -0800

    Add .styles body class to body and all selectors. Resolves #7.

--- classes/styles-css.php
+++ classes/styles-css.php
@@ -8,6 +8,13 @@ class Styles_CSS {
 	var $plugin;
 
 	/**
+	 * Class added to body and all selectors
+	 *
+	 * @var string
+	 */
+	var $body_class = 'styles';
+
+	/**
 	 * @import declarations to be added to top of CSS
 	 *
 	 * @var string
@@ -16,6 +23,22 @@ class Styles_CSS {
 
 	function __construct( $plugin ) {
 		$this->plugin = $plugin;
+
+		add_filter( 'styles_pre_get_css', array( $this, 'selector_prefix' ) );
+		add_filter( 'body_class', array( $this, 'body_class' ) );
+	}
+
+	public function selector_prefix( $element ) {
+		if ( !empty( $element['selector'] ) ) {
+			$element['selector'] = '.' . $this->body_class . ' ' . $element['selector'];
+		}
+
+		return $element;
+	}
+
+	public function body_class( $classes ) {
+		$classes[] = $this->body_class;
+		return $classes;
 	}
 
 	public function output_css() {
@@ -38,6 +61,7 @@ class Styles_CSS {
 				foreach ( $elements as $element ) {
 					if ( $class = Styles_Helpers::get_element_class( $element ) ) {
 
+						$element = apply_filters( 'styles_pre_get_css', $element );
 						$css .= $class::get_css( $group, $element );
 					
 					}
