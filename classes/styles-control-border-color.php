7690f8e52049880dd3dc8ea4f4c06481890af11a
Paul Clark <pdclark@brainstormmedia.com>
2013-04-28 04:16:58 -0800

    Add types: border-color, border-top-color, border-bottom-color, border-right-color, border-left-color

---
+++ classes/styles-control-border-color.php
@@ -0,0 +1,29 @@
+<?php
+
+if ( !class_exists( 'Styles_Control_Color' ) ) {
+	require_once dirname( __FILE__ ) . '/styles-control-color.php';
+}
+
+class Styles_Control_Border_Color extends Styles_Control_Color {
+	var $suffix = 'Border Color';
+	var $property = 'border-color';
+	var $template = '$selector { $property: $value; }';
+
+	public function __construct( $group, $element ) {
+		parent::__construct( $group, $element );
+
+		// Replace $property in $template for child classes
+		$this->template = str_replace( '$property', $this->property, $this->template );
+	}
+
+	public function post_message( $js ) {
+		$js .= str_replace(
+			array( '@setting@', '@selector@', '@property@' ),
+			array( $this->setting, $this->jquery_selector(), $this->property ),
+			file_get_contents( STYLES_DIR . '/js/post-message-part-border-color.js' )
+		);
+
+		return $js . PHP_EOL;
+	}
+
+}
---
+++ classes/styles-control-border-right-color.php
@@ -0,0 +1,12 @@
+<?php
+if ( !class_exists( 'Styles_Control_Border_Color' ) ) {
+	require_once dirname( __FILE__ ) . '/styles-control-border-color.php';
+}
+
+class Styles_Control_Border_Right_Color extends Styles_Control_Border_Color {
+	var $property = 'border-right-color';
+
+	public function __construct( $group, $element ) {
+		parent::__construct( $group, $element );
+	}
+}
