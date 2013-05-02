6adcfd20b4e5d8b2ed9290cfafd164248f3f087b
Cindy Kendrick <cindy@cleverness.org>
2013-05-02 16:00:32 -0400

    border-left-color class contained references to Right instead of Left

--- classes/styles-control-border-left-color.php
+++ classes/styles-control-border-left-color.php
@@ -3,8 +3,8 @@ if ( !class_exists( 'Styles_Control_Border_Color' ) ) {
 	require_once dirname( __FILE__ ) . '/styles-control-border-color.php';
 }
 
-class Styles_Control_Border_Right_Color extends Styles_Control_Border_Color {
-	var $property = 'border-right-color';
+class Styles_Control_Border_Left_Color extends Styles_Control_Border_Color {
+	var $property = 'border-left-color';
 	var $suffix = 'left border color';
 
 	public function __construct( $group, $element ) {
