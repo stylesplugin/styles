2af5aa45e95989ef0c3b63fe095e421f30d4af95
Paul Clark <pdclark@brainstormmedia.com>
2013-05-06 17:50:13 -0800

    Fix plugin style.css appending for every theme

--- classes/styles-child-theme.php
+++ classes/styles-child-theme.php
@@ -9,26 +9,37 @@ class Styles_Child_Theme extends Styles_Child_Updatable {
 		parent::__construct( $args );
 
 		$this->template = str_replace( ' ', '-', strtolower( $this->item_name ) );
-
-		// If styles.css exists in the plugin folder, prepend it to final CSS output
 		$this->styles_css = dirname( $this->plugin_file ) . '/style.css';
-		if ( file_exists( $this->styles_css ) ) {
-			add_filter( 'styles_css_output', array( $this, 'styles_css_output' ) );
+
+		add_filter( 'styles_css_output', array( $this, 'styles_css_output' ) );
+	}
+
+	public function is_active() {
+		if ( Styles_Helpers::get_template() == $this->template ) {
+			return true;
+		}else {
+			return false;
 		}
 	}
 
 	public function get_json_path() {
-		if ( Styles_Helpers::get_template() != $this->template ) {
+		if ( $this->is_active() ) {
+			$json_file = dirname( $this->plugin_file ) . '/customize.json';
+			return $json_file;
+		}else {
 			return false;
 		}
 
-		$json_file = dirname( $this->plugin_file ) . '/customize.json';
-
-		return $json_file;
 	}
 
+	/**
+	 * If styles.css exists in the plugin folder, prepend it to final CSS output
+	 */
 	public function styles_css_output( $css ) {
-		$css = file_get_contents( $this->styles_css ) . $css;
+		if ( $this->is_active() && file_exists( $this->styles_css ) ) {
+			$css = file_get_contents( $this->styles_css ) . $css;
+		}
+
 		return $css;
 	}
 
