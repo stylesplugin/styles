80a7e399cedd62d4dcac72fb015ab61743b6e27a
Paul Clark <pdclark@brainstormmedia.com>
2013-06-24 23:54:17 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -39,14 +39,14 @@ echo "Versions match in readme.txt and PHP file. Let's proceed..."
 cd $GITPATH
 echo -e "Enter a commit message for this new version: \c"
 read COMMITMSG
-git commit -am "$COMMITMSG"
+# git commit -am "$COMMITMSG"
 
-echo "Tagging new version in git"
-git tag -a "$NEWVERSION1" -m "Tagging version $NEWVERSION1"
+# echo "Tagging new version in git"
+# git tag -a "$NEWVERSION1" -m "Tagging version $NEWVERSION1"
 
-echo "Pushing latest commit to origin, with tags"
-git push origin master
-git push origin master --tags
+# echo "Pushing latest commit to origin, with tags"
+# git push origin master
+# git push origin master --tags
 
 echo 
 echo "Creating local copy of SVN repo ..."
@@ -67,11 +67,11 @@ cd $SVNPATH/trunk/
 svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2}' | xargs svn add
 svn commit --username=$SVNUSER -m "$COMMITMSG"
 
-echo "Creating new SVN tag & committing it"
-cd $SVNPATH
-svn copy trunk/ tags/$NEWVERSION1/
-cd $SVNPATH/tags/$NEWVERSION1
-svn commit --username=$SVNUSER -m "Tagging version $NEWVERSION1"
+# echo "Creating new SVN tag & committing it"
+# cd $SVNPATH
+# svn copy trunk/ tags/$NEWVERSION1/
+# cd $SVNPATH/tags/$NEWVERSION1
+# svn commit --username=$SVNUSER -m "Tagging version $NEWVERSION1"
 
 echo "Removing temporary directory $SVNPATH"
 rm -fr $SVNPATH/
