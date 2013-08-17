62639683e1f00187c33db1707c11c17e7745a1a0
Weston Ruter <weston@x-team.com>
2013-08-16 22:13:11 -0600

    Add background-position

---
+++ js/post-message-part-background-position.js
@@ -0,0 +1,32 @@
+(function () {
+	var set_background_position = function () {
+		var x_value = wp.customize.value('@setting@[x_value]')();
+		var x_unit = wp.customize.value('@setting@[x_unit]')();
+		var y_value = wp.customize.value('@setting@[y_value]')();
+		var y_unit = wp.customize.value('@setting@[y_unit]')();
+		var css_property = x_value + x_unit + ' ' + y_value + y_unit;
+		$('@selector@').css( 'background-position', css_property );
+	};
+
+	wp.customize( '@setting@[x_value]', function( value ) {
+		value.bind( function() {
+			set_background_position();
+		} );
+	} );
+	wp.customize( '@setting@[x_unit]', function( value ) {
+		value.bind( function() {
+			set_background_position();
+		} );
+	} );
+	wp.customize( '@setting@[y_value]', function( value ) {
+		value.bind( function() {
+			set_background_position();
+		} );
+	} );
+	wp.customize( '@setting@[y_unit]', function( value ) {
+		value.bind( function() {
+			set_background_position();
+		} );
+	} );
+
+}());
