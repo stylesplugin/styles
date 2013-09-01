15959b4631fb212ba913a04e27a640238bc95cb5
Paul Clark <pdclark@brainstormmedia.com>
2013-08-31 21:30:34 -0800

    Detect plugins based on header name or directory name. Case-insensitive. Resolves #14. Thanks Twitter user @jivedig for the report.

--- classes/styles-child-theme.php
+++ classes/styles-child-theme.php
@@ -10,19 +10,106 @@ class Styles_Child_Theme extends Styles_Child_Updatable {
 
 		$this->template = str_replace( ' ', '-', strtolower( $this->item_name ) );
 		$this->styles_css = dirname( $this->plugin_file ) . '/style.css';
+		$this->plugin_theme_name = trim( str_replace( 'Styles:', '', $this->name ) );
+
+		$this->theme = wp_get_theme();
 
 		add_filter( 'styles_css_output', array( $this, 'styles_css_output' ) );
 	}
 
 	public function is_active() {
-		if (
-			$this->template == Styles_Helpers::get_template()
-			|| $this->template == get_stylesheet()
+
+		// Do parent or child theme header name == Styles plugin "Style Item" header?
+		// WARNING: Don't use this option. It's likely to change.
+		if (    $this->theme_name_equals_plugin_item_name( $this->theme )
+			   || $this->theme_name_equals_plugin_item_name( $this->theme->parent() )
 		) {
 			return true;
-		}else {
-			return false;
 		}
+
+		// Do parent or child theme header name == Styles plugin header name?
+		if (    $this->theme_name_equals_plugin_name( $this->theme )
+			   || $this->theme_name_equals_plugin_name( $this->theme->parent() )
+		) {
+			return true;
+		}
+
+		// Does the parent or child directory name == Styles directory name?
+		if (    $this->theme_directory_name_equals_plugin_directory_name( $this->theme )
+			   || $this->theme_directory_name_equals_plugin_directory_name( $this->theme->parent() )
+		) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Do parent or child theme header name == Styles plugin "Style Item" header?
+	 * (Case insensitive)
+	 *
+	 * For example:
+	 *     Theme Name: Some Parent or Child Theme
+	 *     Styles Item: Some Parent or Child Theme
+	 *
+	 * This is an override for **weird edge cases** where the Styles plugin name
+	 * or folder name can't match the theme name or folder name.
+	 *
+	 * Warning: Don't use this. It's likely to change.
+	 */
+	public function theme_name_equals_plugin_item_name( $theme ) {
+		if ( !is_a( $theme, 'WP_Theme') ) { return false; }
+		if ( 0 === strcasecmp( $this->item_name, $theme->get('Name') ) ) { return true; }
+		return false;
+	}
+
+	/**
+	 * Do parent or child theme header name == Styles plugin header name?
+	 * (Case insensitive)
+	 *
+	 * For example:
+	 *     Theme Name: Some Parent or Child Theme
+	 *     Plugin Name: Styles: Some Parent or Child Theme
+	 *
+	 * ...would return true. 
+	 *
+	 * "Theme Name" is in the theme header.
+	 * "Plugin Name" is in the Styles plugin header.
+	 */
+	public function theme_name_equals_plugin_name( $theme ) {
+		if ( !is_a( $theme, 'WP_Theme') ) { return false; }
+		if ( 0 === strcasecmp( $this->plugin_theme_name, $theme->get('Name') ) ) { return true; }
+		return false;
+	}
+
+	/**
+	 * Does the parent or child directory name == Styles directory name?
+	 * (Case insensitive)
+	 *
+	 * For example:
+	 *     Theme directory: some-parent-or-child-theme
+	 *     Plugin directory: styles-some-parent-or-child-theme
+	 */
+	public function theme_directory_name_equals_plugin_directory_name( $theme ) {
+		if ( !is_a( $theme, 'WP_Theme') ) { return false; }
+		if ( 0 === strcasecmp( $this->get_plugin_directory_name(), $theme->stylesheet ) ) { return true; }
+		return false;
+	}
+
+	public function get_plugin_directory_name() {
+		if ( isset( $this->plugin_directory_name ) ) {
+			return $this->plugin_directory_name;
+		}
+		$plugin_directory_name = basename( dirname( $this->plugin_file ) );
+
+		// Strip 'styles-' from the plugin directory name
+		$remove = 'styles-';
+		if ( $remove == strtolower( substr($plugin_directory_name, 0, strlen( $remove ) ) ) ) {
+			$plugin_directory_name = substr($plugin_directory_name, strlen( $remove ) );
+		}
+
+		$this->plugin_directory_name = $plugin_directory_name;
+		return $this->plugin_directory_name;
 	}
 
 	public function get_json_path() {
